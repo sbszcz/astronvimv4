410c659bc70a0acdea42c4c70c535d65ac193bc5
Sebastian Barszczewski <sebastian.barszczewski@gmail.com>
2024-05-06 10:49:23 +0200

    added incremental selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,5 +11,15 @@ return {
       "templ",
       -- add more arguments for adding more treesitter parsers
     })
+
+    opts.incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    }
   end,
 }
